3ed4f1de2599af11901deaa7dbfccd8bb0e763ca
khalilo2ya <khalilo2y@gmail.com>
2022-04-05 12:47:43 +0100

    done with textbox and label

--- part_manager.py
+++ part_manager.py
@@ -1,7 +1,35 @@
 from tkinter import *
 # create new window
 app=Tk()
+
+#part 
+part_text=StringVar()
+part_label=Label(app, text='Part Name', font=('bold',14), pady=20)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry=Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
+#Customer 
+customer_text=StringVar()
+customer_label=Label(app, text='Customer ', font=('bold',14))
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry=Entry(app, textvariable=customer_text)
+customer_entry.grid(row=0, column=3)
+#Retailer 
+retailer_text=StringVar()
+retailer_label=Label(app, text='Retailer ', font=('bold',14))
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry=Entry(app, textvariable=retailer_text)
+retailer_entry.grid(row=1, column=1)
+#Price 
+price_text=StringVar()
+price_label=Label(app, text='Price ', font=('bold',14))
+price_label.grid(row=1, column=2, sticky=W)
+price_entry=Entry(app, textvariable=price_text)
+price_entry.grid(row=1, column=3)
+
+
 app.title('Part manager')
 app.geometry('700x350')
 #start program
 app.mainloop()
+
